784f83ae314e4d5ab56e6d458b97742ff30b6adb
xiejinqiao <damarkday@163.com>
2025-08-23 18:15:03 +0800

    fix

--- nobody/scene/player/player.gd
+++ nobody/scene/player/player.gd
@@ -13,3 +13,49 @@ var _current_anim = 'down_'
 
 func _ready():
 	pass
+
+func _physics_process(delta: float) -> void:
+	#if PlayerManager.isDeath():
+		#return 
+	
+	
+	var dir = Vector2.ZERO
+	dir.x = Input.get_axis("move_left","move_right")
+	dir.y = Input.get_axis("move_up","move_down")
+	
+	velocity = dir.normalized() *SPEED
+	
+	changeAnim()
+	
+	move_and_slide()
+	
+func get_movement_dir() ->String:
+	#weapon_node.z_index = 1
+	if velocity == Vector2.ZERO:
+		return 'lr_'
+	
+	var angle = velocity.angle()
+	var degree = rad_to_deg(angle)
+	
+	if 45 <=degree and degree <135:
+		return 'down_'
+	elif -135 <=degree and degree <-45:
+		weapon_node.z_index = 0
+		return 'up_'
+	return 'lr_'
+	
+func changeAnim():
+	pass
+	#if velocity == Vector2.ZERO:
+		#anim.play(_current_anim + 'idle')
+	#else:
+		#_current_anim = get_movement_dir()
+		#anim.play(_current_anim + 'move')
+		
+	var mouse_position = get_global_mouse_position()
+	#weapon_node.look_at(_position)
+	
+	#if _position.x>position.x and body.scale.x !=1:
+		#body.scale.x = 1
+	#elif _position.x <position.x && body.scale.x !=-1:
+		#body.scale.x = -1
