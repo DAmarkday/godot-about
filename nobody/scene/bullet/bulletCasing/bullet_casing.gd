beb7e56e25fafb4606684e0b22b3b3cb8f0a670f
xiejinqiao <damarkday@163.com>
2025-10-03 12:49:03 +0800

    fix

--- nobody/scene/bullet/bulletCasing/bullet_casing.gd
+++ nobody/scene/bullet/bulletCasing/bullet_casing.gd
@@ -2,15 +2,20 @@
 extends CharacterBody2D
 @onready var anim:AnimatedSprite2D = $AnimatedSprite2D
 
-@export var gravity: float = 7 
+@export var gravity: float = 9.8
 
 var player_pos:Vector2 = Vector2.ZERO
-var bound:float = randi_range(0,30)      # 范围
+var bound:float = 0     # 落地点的y坐标 初始化为使用者的y坐标
+var setted_bound_range:float = randi_range(-30,30)      # 范围
 var motion: Vector2 = Vector2.ZERO # 初始速度
 var dir:Vector2 = get_random_unit_vector();
 var init_x_speed_counts = 100
 var init_y_speed_counts = 200
-@onready var timer = $Timer
+@onready var timer:Timer = $Timer
+var yTopMax = 0
+
+var topP=0; # top point一定是最小的 初始化为初始位置
+var is_arrived_top:bool = false
 func ani_play():
 	var w=randi_range(1,6)
 	var isFlip=randi_range(0,1)
@@ -19,7 +24,10 @@ func ani_play():
 
 func _ready() -> void:
 	motion = Vector2(dir.x*init_x_speed_counts,dir.y* init_y_speed_counts)
+	bound = GameManager.getPlayerPos().y
+	topP = global_position.y
 	velocity = motion
+	
 	timer.wait_time = 0.2
 	timer.one_shot = false  # 持续触发
 	timer.connect("timeout", ani_play)  # 连接 Timer 的 timeout 信号
@@ -28,34 +36,52 @@ func _ready() -> void:
 func _physics_process(delta):
 	apply_bound(delta)
 
-func get_random_unit_vector(range:Array=[-110, -160]) -> Vector2:
+func get_random_unit_vector(random_range:Array=[-110, -160]) -> Vector2:
 	# 生成 0 到 180 度的随机角度（转换为弧度）
-	var angle_degrees = randf_range(range[0], range[1])
+	var angle_degrees = randf_range(random_range[0], random_range[1])
 	var angle_radians = deg_to_rad(angle_degrees)
 	
 	# 使用 cos 和 sin 计算单位向量的 x 和 y 分量
 	var unit_vector = Vector2(cos(angle_radians), sin(angle_radians))
 	
 	return unit_vector
-
+	
 func apply_bound(_delta):
+	var curP = global_position.y
+	#print('1111 ',curP," ",topP)
+	
+	if topP>=curP:
+		topP = curP
+		is_arrived_top = false
+		print("没有到达顶部")
+		if (abs(motion.y) < 1.0):
+			#到达顶部时暂停
+			timer.paused = true
+	else:
+		if not is_arrived_top:
+			bound = player_pos.y + setted_bound_range
+			is_arrived_top = true
+			print("到达过顶部")
+			if (abs(motion.y) >= 1.0 and not timer.paused):
+				#超过顶部后启动
+				timer.paused = true
+		# 下降
+		pass
+
 	# Should Fall
-	if (global_position.y < player_pos.y + bound):
+	if (global_position.y < bound):
 		motion = motion.move_toward(Vector2(motion.x, motion.y + gravity), gravity)
+		print("下落")
 	# Should bounce
 	else:
-		motion.y = -0.50 * motion.y
+		motion.y = -0.55 * motion.y
 		motion.x = 0.5 * motion.x
-	## Stop physics to prevent performance issues
-	if (abs(motion.y) < 1.0):
-		#到达顶部或底部时都暂停
-		timer.paused = true
-	else:
-		timer.paused = false
-		pass
-		
-	if velocity == Vector2.ZERO:
-		set_physics_process(false)
+		print("反弹")
+		if motion.length()<=1:
+			#到达底部时停止
+			if not timer.is_stopped():
+				timer.stop()
+				set_physics_process(false)
 		
 	velocity = motion
 	move_and_slide()
